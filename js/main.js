4947c0bcf73a8b1897e75ec8f3f543443614de7c
roybrown77 <roybrown77@gmail.com>
2014-06-01 21:44:14 -0400

    add routing

--- js/main.js
+++ js/main.js
@@ -6,17 +6,19 @@ require.config({
     /* Load bootstrap from cdn. On fail, load local file. */
     'bootstrap': ['//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min'],// 'libs/bootstrap-min'],
     'angular': '//code.angularjs.org/1.2.0-rc.2/angular',
-    'angular.route': '//code.angularjs.org/1.2.0-rc.2/angular-route.min'
+    'angular.route': '//code.angularjs.org/1.2.0-rc.2/angular-route.min',
+    'ui.router': '//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.10/angular-ui-router.min'
   },
   shim: {
     'angular' : {'exports' : 'angular'},
     'angular.route': ['angular'],
     'jQuery': {'exports' : 'jQuery'},
-    'bootstrap': ['jQuery']
+    'bootstrap': ['jQuery'],
+    'ui.router': ['angular']
   }
 });
 
-require(['jQuery', 'angular', 'app', 'bootstrap'] , function ($, angular) {
+require(['jQuery', 'angular', 'app', 'bootstrap', 'ui.router'] , function ($, angular) {
   $(function () { // using jQuery because it will run this even if DOM load already happened
     angular.bootstrap(document , ['app']);
   });
--- js/routes.js
+++ js/routes.js
@@ -1,14 +1,18 @@
 define(['angular', 'angular.route', 'controllers'] , function (angular) {
-	angular.module('app.Routes',['ngRoute', 'app.Controllers']).config(function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-    $routeProvider.
-      when('/', {
-          templateUrl: '/index.html',
-          controller: 'HomeCtrl',
-          title: 'Home'
-      }).
-      otherwise({
-        redirectTo: '/index.html'
-      });
+	angular.module('app.Routes',['ngRoute', 'app.Controllers','ui.router']).config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+
+            // HOME STATES AND NESTED VIEWS ========================================
+            .state('home', {
+                url: '/home',
+                templateUrl: 'partial-home.html'
+            })
+
+            // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+            .state('about', {
+                // we'll get to this in a bit
+            });
   });
 });
