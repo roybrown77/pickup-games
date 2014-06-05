0544aa6700c50fdd6ccb9c29c6b641b48b95f464
roybrown77 <roybrown77@gmail.com>
2014-06-05 19:59:09 -0400

    google maps cleanup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 require.config({
   baseUrl: '/js',
   paths: {
-    /* Load jquery from google cdn. On fail, load local file. */
     'jQuery': ['//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min'],// 'libs/jquery-min'],
-    /* Load bootstrap from cdn. On fail, load local file. */
     'bootstrap': ['//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min'],// 'libs/bootstrap-min'],
     'angular': '//code.angularjs.org/1.2.0-rc.2/angular',
     'ui.router': '//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.10/angular-ui-router.min',
-    //'google-maps-api': 'http://maps.googleapis.com/maps/api/js?libraries=places&sensor=false',
     'ngAutocomplete': 'ngAutoComplete'
   },
   shim: {
@@ -15,12 +12,11 @@ require.config({
     'jQuery': {'exports' : 'jQuery'},
     'bootstrap': ['jQuery'],
     'ui.router': ['angular'],
-    //'google-maps-api': ['angular'],
     'ngAutocomplete': ['angular']
   }
 });
 
-require(['jQuery', 'angular', 'app', 'bootstrap', 'ui.router', /*'google-maps-api',*/'ngAutocomplete'] , function ($, angular) {
+require(['jQuery', 'angular', 'app', 'bootstrap', 'ui.router', 'ngAutocomplete'] , function ($, angular) {
   $(function () { // using jQuery because it will run this even if DOM load already happened
     angular.bootstrap(document , ['app']);
   });
