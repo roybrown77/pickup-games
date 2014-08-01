c50b35a70ab13187f5583e4323cfcbccd0b9897c
Roy Brown <roybrown77@gmail.com>
2014-08-01 15:57:08 -0400

    added more features to create and search games

--- dotnet/PickupGames/PickupGames/Models/GameCreateModel.cs
+++ dotnet/PickupGames/PickupGames/Models/GameCreateModel.cs
@@ -12,10 +12,18 @@ namespace PickupGames.Models
         public string Sport { get; set; }
 
         [Required]
+        [Display(Name = "Game Date")]
+        public DateTime GameDate { get; set; }
+
+        [Required]
         [Display(Name = "Game Time")]
         public DateTime GameTime { get; set; }
 
         [Required]
         public string Location { get; set; }
+
+        [Required]
+        [Display(Name = "Recurring Day")]
+        public string RecurringDay { get; set; }
     }
 }
--- dotnet/PickupGames/PickupGames/Models/GameSearchModel.cs
+++ dotnet/PickupGames/PickupGames/Models/GameSearchModel.cs
@@ -9,10 +9,23 @@ namespace PickupGames.Models
 
         public string Sport { get; set; }
 
-        [Display(Name = "Game Time")]
-        public DateTime GameTime { get; set; }
+        [Display(Name = "Recurring Day")]
+        public string GameDay { get; set; }
 
-        public string Location { get; set; }
+        [Display(Name = "Game Date Start")]
+        public DateTime GameDateStart { get; set; }
+
+        [Display(Name = "Game Date End")]
+        public DateTime GameDateEnd { get; set; }
+
+        [Display(Name = "Game Time Start")]
+        public DateTime GameTimeStart { get; set; }
+
+        [Display(Name = "Game Time End")]
+        public DateTime GameTimeEnd { get; set; }
+
+        [Display(Name = "Location")]
+        public string SearchLocation { get; set; }
 
         [Display(Name = "Radius to location")]
         public string Radius { get; set; }
