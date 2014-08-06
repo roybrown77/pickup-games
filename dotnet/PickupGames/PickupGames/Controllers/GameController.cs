a432c13d5c0b4a20c4e8618ad13221539cd8c4d3
Roy Brown <roybrown77@gmail.com>
2014-08-06 16:21:16 -0400

    moved create game to separate window

---
+++ dotnet/PickupGames/PickupGames/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace PickupGames.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("CreateGame");
+        }        
+    }
+}
