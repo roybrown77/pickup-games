0e2446a30195d42f320de561c4584a509139c4d0
Roy Brown <roy.d.brown@ge.com>
2014-10-09 10:36:56 -0400

    code cleanup

--- dotnet/PickupGames/PickupGames/App_Start/SmsService.cs
+++ dotnet/PickupGames/PickupGames/App_Start/SmsService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 
 namespace PickupGames
