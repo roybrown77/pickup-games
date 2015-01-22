5f8beebcc6479434dbd828ae3ffa6ac7a2f5a6ff
Roy Brown <roy.d.brown@ge.com>
2015-01-22 15:38:11 -0500

    add asp.net mvc 5 version

---
+++ dotnet/PickupGames2/PickupGames2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PickupGames2.Startup))]
+namespace PickupGames2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
