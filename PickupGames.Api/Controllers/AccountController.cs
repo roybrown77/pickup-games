5121384204dbed64b2c05e92ecaf4afcb2cacaf1
Roy Brown <roybrown77@gmail.com>
2015-04-08 19:02:02 -0400

    add web api assembly; token authentication

---
+++ PickupGames.Api/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNet.Identity;
+using PickupGames.Api.Repositories;
+using PickupGames.Api.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace PickupGames.Api.Controllers
+{
+    [RoutePrefix("api/v1/Account")]
+    public class AccountController : ApiController
+    {
+        private AuthRepository _repo = null;
+
+        public AccountController()
+        {
+            _repo = new AuthRepository();
+        }
+
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _repo.RegisterUser(userModel);
+
+            IHttpActionResult errorResult = GetErrorResult(result);
+
+            if (errorResult != null)
+            {
+                return errorResult;
+            }
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+}
