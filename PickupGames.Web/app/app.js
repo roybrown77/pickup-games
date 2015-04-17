8648609dca9f83966eafae9a26927f19114e2770
Roy Brown <roybrown77@gmail.com>
2015-04-17 15:28:24 -0400

    ui

--- PickupGames.Web/app/app.js
+++ PickupGames.Web/app/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar', 'ngResource']);
+﻿var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar', 'ngResource', 'ui.bootstrap']);
 
 app.config(function ($routeProvider) {
 
