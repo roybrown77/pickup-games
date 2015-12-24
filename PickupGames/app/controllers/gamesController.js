e47e8e9a2ebd97025706b251439dd726e3cb8002
Roy Brown <rbrown77@gmail.com>
2015-12-23 23:00:49 -0500

    display and scroll results in mobile view

--- PickupGames/app/controllers/gamesController.js
+++ PickupGames/app/controllers/gamesController.js
@@ -10,7 +10,7 @@ app.controller('gamesController', ['$scope', '$http', '$q', '$location', '$resou
             //if (urlSearchParameterArray.length > 0) {
             $location.path("/games/" + location + "/" + index, false).search({ 'zoom': zoom }); //=" + 2 + "&" + urlSearchParameterArray.join("&"));            
             //} else {
-            //    $location.path("/games/usa/1", false).search({ 'zoom' : 4 });;
+            //    $location.path("/games/usa/1", false).search({ 'zoom' : 4 });
             //}  
         } catch (e) {
             var temp = e;
